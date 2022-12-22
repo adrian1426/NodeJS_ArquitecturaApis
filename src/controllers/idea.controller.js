d59a5a9d71c74f718547fca60c11fedf5ab8edd5
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-12-22 12:23:51 -0600

    agregando idea controller

---
+++ src/controllers/idea.controller.js
@@ -0,0 +1,55 @@
+let _ideaService = null;
+
+class IdeaController {
+  // ?IdeaService está registrado en el container
+  constructor({ IdeaService }) {
+    _ideaService = IdeaService;
+  }
+
+  async get(req, res) {
+    const { ideaId } = req.params;
+    const idea = await _ideaService.get(ideaId);
+
+    return res.send(idea);
+  }
+
+  async getAll(req, res) {
+    const idea = await _ideaService.getAll();
+    return res.send(idea);
+  }
+
+  async update(req, res) {
+    const { body } = req;
+    const { ideaId } = req.params;
+
+    const updatedIdea = await _ideaService.update(ideaId, body);
+    return res.send(updatedIdea);
+  }
+
+  async delete(req, res) {
+    const { ideaId } = req.params;
+    const deletedIdea = await _ideaService.delete(ideaId);
+
+    return res.send(deletedIdea);
+  }
+
+  async getUserIdeas(req, res) {
+    const { userId } = req.params;
+    const ideas = await _ideaService.getUserIdeas(userId);
+    return res.send(ideas);
+  }
+
+  async upVoteIdea(req, res) {
+    const { ideaId } = req.params;
+    const idea = await _ideaService.upVoteIdea(ideaId);
+    return res.send(idea);
+  }
+
+  async upDownIdea(req, res) {
+    const { ideaId } = req.params;
+    const idea = await _ideaService.downVoteIdea(ideaId);
+    return res.send(idea);
+  }
+};
+
+module.exports = IdeaController;
